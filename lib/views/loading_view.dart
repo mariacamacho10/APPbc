090e08afe8a33f2d54b066697ac78147e57f0fd3
mariacamacho10 <mariaangelcamacho4@gmail.com>
2025-08-24 13:53:37 -0500

    Agregué vista loading

---
+++ lib/views/loading_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  const Loading ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:[
+            CircularProgressIndicator(),
+            SizedBox(height: 20),
+            Text(
+              'Cargando...',
+              style: TextStyle(fontSize: 16),
+            )
+          ]
+        )
+
+      )
+    )
+  }
+}
