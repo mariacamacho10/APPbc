ea65bef4eefa7ebd7d745d74545d6fb180a3eccf
mariacamacho10 <mariaangelcamacho4@gmail.com>
2025-08-24 23:11:40 -0500

    Agregué vista initial de cubit

---
+++ lib/views/initial_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TareasTerminadasInitialView extends StatelessWidget {
+  const TareasTerminadasInitialView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Lista de tareas terminadas"));
+  }
+}
